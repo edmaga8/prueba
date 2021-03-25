b43437b5b0424e032853cca51fcb9be8604c2415
edmaga8 <edmaga8@gmail.com>
2021-03-25 16:53:27 -0300

    Probando inner

---
+++ FirstClass.py
@@ -0,0 +1,18 @@
+
+def outer_fun(msg):
+    '''Función nulita, solamenta probando algo de First-class'''
+    message=msg
+
+    def inner_fun():
+        print(message)
+    
+    return inner_fun
+
+
+fun= outer_fun('Chola')
+
+'''En este caso se observa que la función de afuera usa los parámetros de la función
+de adentro. 
+'''
+
+print outer_fun().__doc__
